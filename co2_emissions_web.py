3ff4807d6073b53e97fe2cdb11adfc89185d6364
lbibbo6012 <94138091+lbibbo6012@users.noreply.github.com>
2022-12-06 17:05:11 -0500

    Update co2_emissions_web.py

--- co2_emissions_web.py
+++ co2_emissions_web.py
@@ -20,7 +20,7 @@ def create_co2_table(cur, conn):
     conn.commit()
 
 # Change 
-def addEmissionsData(soup, cur, conn):
+def addEmissionsData(countries_lst, soup, cur, conn):
     table = soup.find('table', class_='ecl-table ecl-table--zebra')
     rows = table.find_all('tr', class_='ecl-table__row')
     for row in rows:
@@ -30,10 +30,12 @@ def addEmissionsData(soup, cur, conn):
         if len(row_ind) > 0:
             country = row_ind[0].lower()
             # ask about hardcoding
-            emissions_2020 = row_ind[6]
-            emissions_2021 = row_ind[7]
-            cur.execute('INSERT INTO CO2_Emissions (country, emissions_2020, emissions_2021) VALUES (?,?,?)', (country, emissions_2020, emissions_2021))
-            conn.commit()
+
+            if country in countries_lst:
+                emissions_2020 = row_ind[6]
+                emissions_2021 = row_ind[7]
+                cur.execute('INSERT INTO CO2_Emissions (country, emissions_2020, emissions_2021) VALUES (?,?,?)', (country, emissions_2020, emissions_2021))
+                conn.commit()
 
 class TestCO2Emissions(unittest.TestCase):
     pass
@@ -48,7 +50,15 @@ def main():
     soup = BeautifulSoup(r.text, 'html.parser')
 
     #Call the functions getLink(soup) and getAdmissionsInfo2019(soup) on your soup object.
-    addEmissionsData(soup, cur, conn)
+    countries_lst = [
+    'china', 'united states', 'india', 'russia', 'iran', 'germany', 'south korea', 'indonesia', 'saudi arabia', 'canada',
+    'brazil', 'south africa', 'mexico', 'australia', 'united kingdom', 'vietnam', 'poland', 'taiwan', 'thailand', 'egypt',
+    'malaysia', 'pakistan', 'kazakhstan', 'united arab emirates', 'argentina', 'ukraine', 'iraq', 'algeria', 'philippines', 'netherlands',
+    'nigeria', 'uzbekistan', 'bangladesh', 'venezuela', 'kuwait', 'czechia', 'qatar', 'belgium', 'oman', 'chile', 
+    'turkmenistan', 'romania', 'colombia', 'morocco', 'austria', 'libya', 'north korea', 'belarus', 'singapore', 'peru'
+    ]
+
+    addEmissionsData(countries_lst, soup, cur, conn)
 
 
 if __name__ == "__main__":
