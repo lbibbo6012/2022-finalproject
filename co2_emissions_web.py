201ce97111693cc1bdb84a2301b8780803d340e7
lbibbo6012 <94138091+lbibbo6012@users.noreply.github.com>
2022-12-06 14:43:16 -0500

    Update co2_emissions_web.py

--- co2_emissions_web.py
+++ co2_emissions_web.py
@@ -5,6 +5,8 @@ import os
 import matplotlib.pyplot as plt
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+
 
 # Create Database
 def setUpDatabase(db_name):
@@ -15,20 +17,24 @@ def setUpDatabase(db_name):
 
 def create_co2_table(cur, conn):
     cur.execute("DROP TABLE IF EXISTS CO2_Emissions")
-    cur.execute('CREATE TABLE IF NOT EXISTS CO2_Emissions (country TEXT, year INTEGER, total_cases INTEGER)')
+    cur.execute('CREATE TABLE IF NOT EXISTS CO2_Emissions (country TEXT, emissions_2020 NUMBER, emissions_2021 NUMBER)')
     conn.commit()
 
 # Change 
-def getEmissionsData(soup):
-    d = {}
-    div = soup.find('div', class_='wp-block-column')
-    table = soup.find()
-    for row in table.find_all('tr'):
+def addEmissionsData(soup, cur, conn):
+    table = soup.find('table', class_='ecl-table ecl-table--zebra')
+    rows = table.find_all('tr', class_='ecl-table__row')
+    for row in rows:
         line = row.find_all('td')
-        print(line)
-        d[line[0].text.replace('\\n', '').strip()] = line[1].text.replace('\\n', '').strip()
-    return d
+        row_ind = [i.text for i in line]
 
+        if len(row_ind) > 0:
+            country = row_ind[0].lower()
+            # ask about hardcoding
+            emissions_2020 = row_ind[6]
+            emissions_2021 = row_ind[7]
+            cur.execute('INSERT INTO CO2_Emissions (country, emissions_2020, emissions_2021) VALUES (?,?,?)', (country, emissions_2020, emissions_2021))
+            conn.commit()
 
 class TestCO2Emissions(unittest.TestCase):
     pass
@@ -36,15 +42,17 @@ class TestCO2Emissions(unittest.TestCase):
 def main():
     # SETUP DATABASE AND TABLE
     cur, conn = setUpDatabase('api_data.db')
+    create_co2_table(cur, conn)
 
-    url = 'https://ourworldindata.org/co2-emissions'
+    url = 'https://edgar.jrc.ec.europa.eu/report_2022'
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
 
     #Call the functions getLink(soup) and getAdmissionsInfo2019(soup) on your soup object.
-    getEmissionsData(soup)
+    addEmissionsData(soup, cur, conn)
 
 
 if __name__ == "__main__":
     main()
     unittest.main(verbosity=2)
+
